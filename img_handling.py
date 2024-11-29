940e89c5f7e2613af1156abbd8e79894986f73e6
codingkitten4 <saudkhan10@hotmail.com>
2024-11-29 16:11:25 -0500

    sharpen the image

--- img_handling.py
+++ img_handling.py
@@ -4,19 +4,24 @@ import time, os
 from PIL import Image
 
 def import_image():
+    for i in range(4):
+        print(i+1)
+        time.sleep(1) #wait for 4 seconds to adjust the camera
+        
     cap = cv.VideoCapture(0)
     ret, frame = cap.read()
-    time.sleep(2) #wait for 2 seconds to adjust the camera
-        
-    cv.imwrite(r'images\unchanged.jpg',frame)
+
+    cv.imwrite(r'images/unchanged.jpg',frame)
     cv.destroyAllWindows()
     cap.release()
 
 import_image()
 
-pyts.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pyts.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-img = Image.open(r"images\unchanged.jpg")
+img = Image.open(r"images/unchanged.jpg")
+
+img.save('images/unchanged_sharp.jpg', quality=95)
 text = pyts.image_to_string(img)
 
 print(text)
