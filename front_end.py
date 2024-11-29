ead623226f4d717ed1bae2f94c7acdfd09125ed6
codingkitten4 <saudkhan10@hotmail.com>
2024-11-29 17:12:18 -0500

    push again

--- front_end.py
+++ front_end.py
@@ -1,12 +1,10 @@
 import customtkinter
-
-def button_callback():
-    print("button clicked")
+from gemini_file import summarize_image
 
 app = customtkinter.CTk()
 app.geometry("400x150")
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button = customtkinter.CTkButton(app, text="summarize", command=summarize_image)
 button.pack(padx=20, pady=20)
 
 app.mainloop()
